b561ed98a524200b51b5ced80bd860091a857951
AdhvikVithun <tadhvik@karunya.edu.in>
2023-12-29 21:49:44 +0530

    Version 2.0 Works on Test

--- combexcell.py
+++ combexcell.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 # Read the data from the first sheet and the first column of the Excel file
 input_file_path = "D:\\adhvik\\adh\\dad project\\4000.xlsx"
@@ -6,59 +7,74 @@ df = pd.read_excel(input_file_path, header=None, sheet_name=0, usecols=[0])
 
 # Convert each cell to strings
 df = df.applymap(str)
-print("Df ")
-print(df)
 
 # Initialize a list to store dictionaries, each representing a row in the output DataFrame
 output_data = []
-
-# Initialize variables to store values for each column
-ward = ""
-area = ""
 
 # Iterate through each row in the input data
 for index, row in df.iterrows():
     data = row.iloc[0]  # Assuming the data is in the first (and only) column
 
-    # Check for the presence of வார்டு and update the Ward and Area accordingly
-    if "வார்டு" in data:
-        ward = data.split()[-1]
-        area = data.split("வார்டு")[-1].strip()
-        continue
+    # Extract serial number if present
+    serial_match = re.search(r'^\s*(\d+)\s*\.', data)
+    if serial_match:
+        serial_no = serial_match.group(1).strip()
 
-    # Extract information based on the provided rules
-    parts = data.split(":", 2)
-    if len(parts) == 3 and (parts[0].startswith("WTD") or parts[0].startswith("JBB")):
-        serial_no, voter_info, rest = parts
-        voter_id = voter_info.split()[0]
-        name = voter_info.split(":")[1].split()[0]
-        father_husband_name = voter_info.split(":")[2].split()[0]
+        # Extract Voter ID if present
+        voter_id_match = re.search(r'\b([A-Z]+\d+)\b', data)
+        voter_id = voter_id_match.group(1).strip() if voter_id_match else ""
+
+        # Extract Name if present, excluding words "கணவர்" or "தந்தை"
+        name_match = re.search(rf'{voter_id}\s*:\s*((?:(?!கணவர்|தந்தை).)+)\s*', data)
+        name = name_match.group(1).strip() if name_match else ""
+
+        # Extract Father/Husband Name if present after "கணவர்" or "தந்தை"
+        father_husband_name_match = re.search(r'(?:கணவர்|தந்தை)\s*:\s*(.+?)\s*(?:(?:\d+\s*\.\s*)|$)', data)
+        father_husband_name = father_husband_name_match.group(1).strip() if father_husband_name_match else ""
 
         # Create a dictionary representing a row in the output DataFrame
         row_data = {
-            "Ward": ward,
-            "Area": area,
-            "Serial No": serial_no,
+            "Ward": "",  # Add appropriate values for these columns
+            "Election": "",
+            "Booth": "",
             "Voter ID": voter_id,
+            "Serial No": serial_no,
+            "Part No": "",
+            "Page No": "",
+            "Male/Female": "",
+            "Age": "",
+            "Phone Num": "",
+            "Aadhar Num": "",
             "Name": name,
             "Father/Husband Name": father_husband_name,
+            "Door Num": "",
+            "Address": "",
+            "Family": "",
+            "Caste": "",
+            "Area Guide": "",
+            "Guide Number": "",
+            "Event": "",
+            "Memo": "",
+            "Qualification": "",
+            "Stay": "",
+            "Voted": "",
+            "Voted Date": "",
         }
 
         # Append the row data to the output list
         output_data.append(row_data)
 
-# Print some intermediate information
-print("Output Data:")
-print(output_data)
-
 # Create a DataFrame from the list of dictionaries
 output_df = pd.DataFrame(output_data)
 
-# Print the DataFrame
-print("Output DataFrame:")
-print(output_df)
+# Reorder columns to match the specified order
+column_order = ["Ward", "Election", "Booth", "Voter ID", "Serial No", "Part No", "Page No", 
+                "Male/Female", "Age", "Phone Num", "Aadhar Num", "Name", "Father/Husband Name",
+                "Door Num", "Address", "Family", "Caste", "Area Guide", "Guide Number", 
+                "Event", "Memo", "Qualification", "Stay", "Voted", "Voted Date"]
+
+output_df = output_df[column_order]
 
 # Write the transformed data to the second Excel sheet
 output_file_path = "D:\\adhvik\\adh\\dad project\\100A Test.xlsx"
 output_df.to_excel(output_file_path, index=False)
-
